8c11b98903225a44dc9dfff49725baa5ee79ecb0
Lowe, Tom (Data61, Pullenvale) <Thomas.Lowe@data61.csiro.au>
2022-01-28 16:42:41 +1000

    added treesplit as a helper

---
+++ treetools/treesplit/treesplit.cpp
@@ -0,0 +1,106 @@
+// Copyright (c) 2022
+// Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+// ABN 41 687 119 230
+//
+// Author: Thomas Lowe
+#include "treelib/treeutils.h"
+#include <raylib/rayforestgen.h>
+#include <raylib/raytreegen.h>
+#include <raylib/rayparse.h>
+#include <raylib/raycloud.h>
+#include <raylib/rayrenderer.h>
+#include "raylib/raytreegen.h"
+#include <cstdlib>
+#include <iostream>
+
+void usage(int exit_code = 1)
+{
+  std::cout << "Split a tree file around a criterion" << std::endl;
+  std::cout << "usage:" << std::endl;
+  std::cout << "treesplit forest.txt radius 0.1   - split around tree radius" << std::endl;
+  std::cout << "                     plane 0,0,1  - split around horizontal plane at height 1" << std::endl;
+  std::cout << "                     colour 0,0,1 - split around colour green value 1" << std::endl;
+  std::cout << "                     box rx,ry,rz - split around a centred box of the given radii" << std::endl;
+  exit(exit_code);
+}
+
+// Read in a ray cloud and convert it into an array for topological optimisation
+int main(int argc, char *argv[])
+{
+  ray::FileArgument forest_file;
+  ray::Vector3dArgument coord;
+
+  ray::DoubleArgument radius(0.001,1000.0);
+  ray::Vector3dArgument plane, colour, box;
+  ray::KeyValueChoice choice({"plane", "colour", "radius", "box"}, 
+                             {&plane, &colour,  &radius, &box});
+
+  bool parsed = ray::parseCommandLine(argc, argv, {&forest_file, &choice});
+  if (!parsed)
+  {
+    usage();
+  }
+
+  ray::ForestStructure forest;
+  if (!forest.load(forest_file.name()))
+  {
+    usage();
+  }  
+
+  ray::ForestStructure forest_in, forest_out;
+  if (choice.selectedKey() == "radius")
+  {
+    for (auto &tree: forest.trees)
+    {
+      if (tree.segments()[0].radius < radius.value())
+        forest_in.trees.push_back(tree);
+      else
+        forest_out.trees.push_back(tree);
+    }
+  }
+  else if (choice.selectedKey() == "plane")
+  {
+    Eigen::Vector3d plane_vec = plane.value() / plane.value().squaredNorm();
+    for (auto &tree: forest.trees)
+    {
+      if (tree.segments()[0].tip.dot(plane_vec) < 1.0)
+        forest_in.trees.push_back(tree);
+      else
+        forest_out.trees.push_back(tree);
+    }
+  }  
+  else if (choice.selectedKey() == "colour")
+  {
+    Eigen::Vector3d colour_vec = colour.value() / colour.value().squaredNorm();
+    for (auto &tree: forest.trees)
+    {
+      const auto &it = std::find(tree.attributes().begin(), tree.attributes().end(), "red");
+      if (it != tree.attributes().end())
+      {
+        int red_index = (int)(it - tree.attributes().begin()); 
+        auto &ats = tree.segments()[0].attributes;
+        Eigen::Vector3d col(ats[red_index], ats[red_index+1], ats[red_index+2]); // we assume green, blue follow on consecutively 
+        if (col.dot(colour_vec) < 1.0)
+          forest_in.trees.push_back(tree);
+        else
+          forest_out.trees.push_back(tree);
+      }
+    }
+  }
+  else if (choice.selectedKey() == "box")
+  {
+    for (auto &tree: forest.trees)
+    {
+      auto &pos = tree.segments()[0].tip;
+      if (std::abs(pos[0]) < box.value()[0] && std::abs(pos[1]) < box.value()[1] && std::abs(pos[2]) < box.value()[2])
+        forest_in.trees.push_back(tree);
+      else
+        forest_out.trees.push_back(tree);
+    }
+  } 
+
+  forest_in.save(forest_file.nameStub() + "_inside.txt");
+  forest_out.save(forest_file.nameStub() + "_outside.txt");
+}
+
+
