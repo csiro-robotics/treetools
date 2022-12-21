b5e0ff59b738b2dbc7778e7d3841f92430b12950
Lowe, Tom (Data61, Pullenvale) <Thomas.Lowe@data61.csiro.au>
2022-12-21 10:07:40 +1000

    working better

--- treetools/treemesh/treemesh.cpp
+++ treetools/treemesh/treemesh.cpp
@@ -26,179 +26,11 @@ void usage(int exit_code = 1)
   exit(exit_code);
 }
 
-// add a cylinder to the @c mesh
-void addCylinder(ray::Mesh &mesh, const Eigen::Vector3d &pos1, const Eigen::Vector3d &pos2, double radius,
-                 ray::RGBA rgba)
-{
-  const int n = (int)mesh.vertices().size();
-  const Eigen::Vector3i N(n, n, n);
-  std::vector<Eigen::Vector3i> &indices = mesh.indexList();
-  std::vector<Eigen::Vector3d> vertices(14);
-
-  const Eigen::Vector3d dir = (pos2 - pos1).normalized();
-  const Eigen::Vector3d diag(1, 2, 3);
-  const Eigen::Vector3d side1 = dir.cross(diag).normalized();
-  const Eigen::Vector3d side2 = side1.cross(dir);
-
-  vertices[12] = pos1 - radius * dir;
-  vertices[13] = pos2 + radius * dir;
-  for (int i = 0; i < 6; i++)
-  {
-    const double pi = 3.14156;
-    const double angle = (double)i * 2.0 * pi / 6.0;
-    const double angle2 = angle + pi / 6.0;
-    vertices[i] = pos1 + radius * (side1 * std::sin(angle) + side2 * std::cos(angle));
-    vertices[i + 6] = pos2 + radius * (side1 * std::sin(angle2) + side2 * std::cos(angle2));
-    indices.push_back(N + Eigen::Vector3i(12, i, (i + 1) % 6));  // start end
-    indices.push_back(N + Eigen::Vector3i(13, (i + 1) % 6 + 6, i + 6));
-    indices.push_back(N + Eigen::Vector3i(i, i + 6, (i + 1) % 6));
-    indices.push_back(N + Eigen::Vector3i((i + 1) % 6 + 6, (i + 1) % 6, i + 6));
-  }
-  mesh.vertices().insert(mesh.vertices().end(), vertices.begin(), vertices.end());
-  for (size_t i = 0; i < vertices.size(); i++)
-  {
-    mesh.colours().push_back(rgba);
-  }
-}
-
-void addOpenCylinder(ray::Mesh &mesh, const Eigen::Vector3d &pos1, const Eigen::Vector3d &pos2, 
-                 double radius, double top_radius,
-                 const Eigen::Vector3d &side1, const Eigen::Vector3d &side2, 
-                 const Eigen::Vector3d &top_side1, const Eigen::Vector3d &top_side2, ray::RGBA rgba,
-                 bool cap_start, bool cap_end)
-{
-  const int n = (int)mesh.vertices().size();
-  const Eigen::Vector3i N(n, n, n);
-  std::vector<Eigen::Vector3i> &indices = mesh.indexList();
-  int num_verts = 12;
-  if (cap_start)
-    num_verts++;
-  if (cap_end)
-    num_verts++;
-  std::vector<Eigen::Vector3d> vertices(num_verts);
-  Eigen::Vector3d dir = (pos2 - pos1).normalized();
-  int last = 12;
-  if (cap_start)
-    vertices[last++] = pos1 - radius * dir;
-  if (cap_end)
-    vertices[last++] = pos2 + top_radius * dir;
-
-  for (int i = 0; i < 6; i++)
-  {
-    const double pi = 3.14156;
-    const double angle = (double)i * 2.0 * pi / 6.0;
-    const double angle2 = angle + pi / 6.0;
-    vertices[i] = pos1 + radius * (side1 * std::sin(angle) + side2 * std::cos(angle));
-    vertices[i + 6] = pos2 + top_radius * (top_side1 * std::sin(angle2) + top_side2 * std::cos(angle2));
-    indices.push_back(N + Eigen::Vector3i(i, i + 6, (i + 1) % 6));
-    indices.push_back(N + Eigen::Vector3i((i + 1) % 6 + 6, (i + 1) % 6, i + 6));
-
-    int last = 12;
-    if (cap_start)
-      indices.push_back(N + Eigen::Vector3i(last++, i, (i + 1) % 6));  
-    if (cap_end)
-      indices.push_back(N + Eigen::Vector3i(last++, (i + 1) % 6 + 6, i + 6));
-  }
-  mesh.vertices().insert(mesh.vertices().end(), vertices.begin(), vertices.end());
-  for (size_t i = 0; i < vertices.size(); i++)
-  {
-    mesh.colours().push_back(rgba);
-  }
-}
-
-
-void generateSmoothMesh(ray::Mesh &mesh, const ray::ForestStructure &forest)
-{
-  for (const auto &tree : forest.trees)
-  {
-    const auto &segments = tree.segments();
-    std::vector<std::vector<int> > children(segments.size());
-    for (size_t i = 0; i < segments.size(); i++)
-    {
-      const auto &segment = segments[i];
-      int parent = segment.parent_id;
-      if (parent != -1)
-      {
-        children[parent].push_back(i);
-      }
-    }
-    std::vector<int> roots;
-    for (size_t i = 1; i < segments.size(); i++)
-    {
-      if (segments[i].parent_id > 0)
-      {
-        break;
-      }
-      roots.push_back(i);
-    }
-    ray::RGBA rgba(127, 127, 127, 255);
-    // for each root
-    for (size_t i = 0; i < roots.size(); i++)
-    {
-      int root_id = roots[i];
-      Eigen::Vector3d normal(1,2,3); // unlikely that any cylinder directions will exactly match this
-      Eigen::Vector3d last_dir = (segments[root_id].tip - segments[segments[root_id].parent_id].tip).normalized();
-   //   Eigen::Vector3d last_axis1 = normal.cross(last_dir).normalized();
-   //   Eigen::Vector3d last_axis2 = last_axis1.cross(last_dir);
-
-      std::vector<int> childlist = {root_id};
-      for (size_t j = 0; j<childlist.size(); j++)
-      {
-        int child_id = childlist[j];
-        int par_id = segments[child_id].parent_id;
-        Eigen::Vector3d dir = (segments[child_id].tip - segments[par_id].tip).normalized();
-        Eigen::Vector3d last_axis1 = normal.cross(last_dir).normalized();
-        Eigen::Vector3d last_axis2 = last_axis1.cross(last_dir);
-        
-        double parent_rad = (child_id != root_id) ? segments[par_id].radius : segments[child_id].radius;
-
-        std::vector<int> kids = children[child_id];
-        if (kids.empty())
-        {
-          Eigen::Vector3d axis1 = normal.cross(dir).normalized();
-          Eigen::Vector3d axis2 = last_axis1.cross(dir);
-          addOpenCylinder(mesh, segments[par_id].tip, segments[child_id].tip, parent_rad, segments[child_id].radius, 
-            last_axis1, last_axis2, axis1, axis2, rgba, child_id==root_id, true);
-          break;
-        }
-        double max_rad = 0.0;
-        int max_k = 0;
-        for (size_t k = 0; k<kids.size(); k++)
-        {
-          double rad = segments[kids[k]].radius;
-          if (rad > max_rad)
-          {
-            max_rad = rad;
-            max_k = k;
-          }
-        }
-        for (size_t k = 0; k<kids.size(); k++)
-        {
-          if (k != max_k)
-          {
-            roots.push_back(kids[k]);
-          }
-        }
-
-        int next_id = kids[max_k];
-        Eigen::Vector3d dir2 = (segments[next_id].tip - segments[child_id].tip).normalized();
-        
-        Eigen::Vector3d top_dir = (dir2 + dir).normalized();
-        Eigen::Vector3d axis1 = normal.cross(top_dir).normalized();
-        Eigen::Vector3d axis2 = last_axis1.cross(top_dir);
-        normal = axis1;
-
-        addOpenCylinder(mesh, segments[par_id].tip, segments[child_id].tip, parent_rad, segments[child_id].radius, 
-          last_axis1, last_axis2, axis1, axis2, rgba, child_id==root_id, false);
-        
-        last_dir = top_dir;
-
-        childlist.push_back(kids[max_k]);
-      }
-    }
-  }
-}
-
+// forward declarations
+void addCylinder(ray::Mesh &mesh, const Eigen::Vector3d &pos1, const Eigen::Vector3d &pos2, double radius, ray::RGBA rgba);
+void addCylinderPiece(ray::Mesh &mesh, int wind, const Eigen::Vector3d &pos, const Eigen::Vector3d &side1, 
+  const Eigen::Vector3d &side2, double radius, const ray::RGBA &rgba, bool cap_start, bool cap_end);
+void generateSmoothMesh(ray::Mesh &mesh, const ray::ForestStructure &forest);
 
 /// This method converts the tree file into a .ply mesh structure, with one cylinder approximation 
 /// per segment, coloured according to the tree file's colour attributes. 
@@ -325,4 +157,172 @@ int main(int argc, char *argv[])
     return system(("meshlab " + forest_file.nameStub() + "_mesh.ply").c_str());
   }
   return 0;
+}
+
+// add a cylinder to the @c mesh
+void addCylinder(ray::Mesh &mesh, const Eigen::Vector3d &pos1, const Eigen::Vector3d &pos2, double radius,
+                 ray::RGBA rgba)
+{
+  const int n = (int)mesh.vertices().size();
+  const Eigen::Vector3i N(n, n, n);
+  std::vector<Eigen::Vector3i> &indices = mesh.indexList();
+  std::vector<Eigen::Vector3d> vertices(14);
+
+  const Eigen::Vector3d dir = (pos2 - pos1).normalized();
+  const Eigen::Vector3d diag(1, 2, 3);
+  const Eigen::Vector3d side1 = dir.cross(diag).normalized();
+  const Eigen::Vector3d side2 = side1.cross(dir);
+
+  vertices[12] = pos1 - radius * dir;
+  vertices[13] = pos2 + radius * dir;
+  for (int i = 0; i < 6; i++)
+  {
+    const double pi = 3.14156;
+    const double angle = (double)i * 2.0 * pi / 6.0;
+    const double angle2 = angle + pi / 6.0;
+    vertices[i] = pos1 + radius * (side1 * std::sin(angle) + side2 * std::cos(angle));
+    vertices[i + 6] = pos2 + radius * (side1 * std::sin(angle2) + side2 * std::cos(angle2));
+    indices.push_back(N + Eigen::Vector3i(12, i, (i + 1) % 6));  // start end
+    indices.push_back(N + Eigen::Vector3i(13, (i + 1) % 6 + 6, i + 6));
+    indices.push_back(N + Eigen::Vector3i(i, i + 6, (i + 1) % 6));
+    indices.push_back(N + Eigen::Vector3i((i + 1) % 6 + 6, (i + 1) % 6, i + 6));
+  }
+  mesh.vertices().insert(mesh.vertices().end(), vertices.begin(), vertices.end());
+  for (size_t i = 0; i < vertices.size(); i++)
+  {
+    mesh.colours().push_back(rgba);
+  }
+}
+
+// add a single section of a cylinder. Each one is like a node in the polyline with a radius.
+void addCylinderPiece(ray::Mesh &mesh, int wind, const Eigen::Vector3d &pos, 
+  const Eigen::Vector3d &side1, const Eigen::Vector3d &side2, double radius, const ray::RGBA &rgba,
+                 bool cap_start, bool cap_end)
+{
+  const int n = (int)mesh.vertices().size();
+  const Eigen::Vector3i N(n, n, n);
+  std::vector<Eigen::Vector3i> &indices = mesh.indexList();
+  std::vector<Eigen::Vector3d> vertices;
+  vertices.reserve(7);
+  Eigen::Vector3d dir = side2.cross(side1);
+  if (cap_start)
+    vertices.push_back(pos - radius * dir);
+
+  for (int i = 0; i < 6; i++)
+  {
+    const double pi = 3.14156;
+    double angle = ((double)i * 2.0 + (double)wind) * pi / 6.0;
+
+    vertices.push_back(pos + radius * (side1 * std::sin(angle) + side2 * std::cos(angle)));
+    if (cap_start)
+    {
+      indices.push_back(N + Eigen::Vector3i(0, 1+i, 1+((i + 1) % 6)));  
+    }
+    else
+    {
+      indices.push_back(N + Eigen::Vector3i(i-6, i, ((i + 1) % 6) - 6));
+      indices.push_back(N + Eigen::Vector3i((i + 1) % 6, ((i + 1) % 6)-6, i));
+    }
+  }
+  if (cap_end)
+  {
+    vertices.push_back(pos + radius * dir);
+    for (int i = 0; i < 6; i++)
+    {
+      indices.push_back(N + Eigen::Vector3i(6, (i + 1) % 6, i));
+    }
+  }
+  mesh.vertices().insert(mesh.vertices().end(), vertices.begin(), vertices.end());
+  for (size_t i = 0; i < vertices.size(); i++)
+  {
+    mesh.colours().push_back(rgba);
+  }
+}
+
+void generateSmoothMesh(ray::Mesh &mesh, const ray::ForestStructure &forest)
+{
+  for (const auto &tree : forest.trees)
+  {
+    const auto &segments = tree.segments();
+    std::vector<std::vector<int> > children(segments.size());
+    for (size_t i = 0; i < segments.size(); i++)
+    {
+      const auto &segment = segments[i];
+      int parent = segment.parent_id;
+      if (parent != -1)
+      {
+        children[parent].push_back(i);
+      }
+    }
+    std::vector<int> roots;
+    for (size_t i = 1; i < segments.size(); i++)
+    {
+      if (segments[i].parent_id > 0)
+      {
+        break;
+      }
+      roots.push_back(i);
+    }
+    ray::RGBA rgba(127, 127, 127, 255);
+    // for each root
+    for (size_t i = 0; i < roots.size(); i++)
+    {
+      int root_id = roots[i];
+      Eigen::Vector3d normal(1,2,3); // unlikely that any cylinder directions will exactly match this
+
+      std::vector<int> childlist = {root_id};
+      int wind = 0;
+      for (size_t j = 0; j<childlist.size(); j++)
+      {
+        int child_id = childlist[j];
+        int par_id = segments[child_id].parent_id;
+        Eigen::Vector3d dir = (segments[child_id].tip - segments[par_id].tip).normalized();
+
+        Eigen::Vector3d axis1 = normal.cross(dir).normalized();
+        Eigen::Vector3d axis2 = axis1.cross(dir);
+        if (child_id == root_id)
+        {
+          addCylinderPiece(mesh, wind, segments[par_id].tip, axis1, axis2, segments[child_id].radius, rgba, true, false);
+        }
+
+        wind++;
+        std::vector<int> kids = children[child_id];
+        if (kids.empty())
+        {
+          addCylinderPiece(mesh, wind, segments[child_id].tip, axis1, axis2, segments[child_id].radius, rgba, false, true);
+          break;
+        }
+        double max_rad = 0.0;
+        int max_k = 0;
+        for (size_t k = 0; k<kids.size(); k++)
+        {
+          double rad = segments[kids[k]].radius;
+          if (rad > max_rad)
+          {
+            max_rad = rad;
+            max_k = k;
+          }
+        }
+        for (size_t k = 0; k<kids.size(); k++)
+        {
+          if (k != max_k)
+          {
+            roots.push_back(kids[k]);
+          }
+        }
+
+        int next_id = kids[max_k];
+        Eigen::Vector3d dir2 = (segments[next_id].tip - segments[child_id].tip).normalized();
+        
+        Eigen::Vector3d top_dir = (dir2 + dir).normalized();
+        Eigen::Vector3d mid_axis1 = normal.cross(top_dir).normalized();
+        Eigen::Vector3d mid_axis2 = mid_axis1.cross(top_dir);
+        normal = -mid_axis2;
+        
+        addCylinderPiece(mesh, wind, segments[child_id].tip, mid_axis1, mid_axis2, segments[child_id].radius, rgba, false, false);
+
+        childlist.push_back(kids[max_k]);
+      }
+    }
+  }
 }
