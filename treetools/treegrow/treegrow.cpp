60e38e155762ee5d94c9e7a5503670ce35e7d4b5
Thomas Lowe <thomas.lowe@csiro.au>
2023-05-08 10:53:15 +1000

    clean

--- treetools/treegrow/treegrow.cpp
+++ treetools/treegrow/treegrow.cpp
@@ -19,7 +19,7 @@ void usage(int exit_code = 1)
   std::cout << "usage:" << std::endl;
   std::cout << "treegrow forest.txt 1 years             - reduce the tree according to the rates, default values below." << std::endl;
   std::cout << "                    --length_rate 0.3   - expected branch length increase per year in m" << std::endl;
-  std::cout << "                    --shed              - shed branches annually to maintain branch length power law" << std::endl;
+  std::cout << "                    --shed              - shed branches to maintain branch length power law" << std::endl;
   std::cout << "                    --prune_length 1    - length from tip that reconstructed trees are pruned to" << std::endl;
   // clang-format on
   exit(exit_code);
@@ -114,7 +114,6 @@ int main(int argc, char *argv[])
     // 4. dominance
     auto &at_names = forest.trees[0].treeAttributeNames();
     auto &root_at_names = forest.trees[0].attributeNames();
-  //    int height_id = static_cast<int>(std::find(at_names.begin(), at_names.end(), "height") - at_names.begin());
     const int dimension_id = static_cast<int>(std::find(at_names.begin(), at_names.end(), "dimension") - at_names.begin());
     const int angle_id = static_cast<int>(std::find(root_at_names.begin(), root_at_names.end(), "angle") - root_at_names.begin());
     const int length_id = static_cast<int>(std::find(root_at_names.begin(), root_at_names.end(), "length") - root_at_names.begin());
@@ -134,112 +133,109 @@ int main(int argc, char *argv[])
     // 6. finally, re-adjust the segment lengths to have the same rough ratio of radius to length. Don't move the branch points
     const double prune_length = prune_length_option.isSet() ? prune_length_argument.value() : 1.0;
 
-  //  for (int i = 0; i<static_cast<int>(period.value()); i++)
+    for (auto &tree : forest.trees)
     {
-      for (auto &tree : forest.trees)
+      std::vector<int> children(tree.segments().size(), 0);
+      for (size_t j = 0; j<tree.segments().size(); j++)
       {
-        std::vector<int> children(tree.segments().size(), 0);
-        for (size_t j = 0; j<tree.segments().size(); j++)
+        int par = tree.segments()[j].parent_id;
+        if (par != -1)
         {
-          int par = tree.segments()[j].parent_id;
-          if (par != -1)
-          {
-            children[par]++;
-          }
-        }
-        auto &root = tree.segments()[0];
-        double dimension = tree.treeAttributes()[dimension_id];
-        double branch_angle = root.attributes[angle_id] * ray::kPi/180.0;
-        double radius = root.radius;
-        double length = root.attributes[length_id];
-        double dominance = root.attributes[dominance_id];
-
-        const double radius_growth = length_growth * radius / length;
-
-        // convet dimension to the average downscale at each branch point
-        const double k = std::pow(2.0, -1.0/dimension);
-        // use dominance to work out the large and small downscale...
-        // d1 * d2 = k*k
-        // dominance = -1.0 + 2.0 * sqr(max_rad) / (sqr(max_rad) + sqr(second_max_rad));
-        double area_ratio = (dominance + 1.0) / 2.0; // big child area per parent area
-        double d1 = std::sqrt(area_ratio); // big child radius for unit parent radius
-        double d2 = std::sqrt(1.0 - area_ratio); // small child radius for unit parent radius
-        // if c^2 d1 d2 = k^2
-        double d_scale = k / std::sqrt(d1 * d2);
-        std::cout << "d1: " << d1 << ", d2: " << d2 << ", d_scale: " << d_scale << std::endl;
-        double k1 = d1 * d_scale;
-        double k2 = d2 * d_scale;
-
-        // what are branch angles 1 and 2? We know the dominance and overall branch angle...
-        // angle1 + angle2 = branch_angle
-        // tan(angle1)/rad2^2 = tan(angle2)/rad1^2
-        // tan(angle1)/tan(angle2) = (rad2 / rad1)^2
-        double angle1 = branch_angle/2.0;
-        for (int i = 0; i<20; i++) // no closed form expression, so iterate
-        {
-          angle1 = std::atan( std::tan(branch_angle - angle1) * ray::sqr(k2/k1));
-        }  
-        std::cout << "dominant angle: " << angle1 << ", total angle: " << branch_angle << std::endl;
-
-        // 1. add subtrees at each leaf point....
-        size_t num_segs = tree.segments().size();
-        for (size_t i = 0; i<num_segs; i++)
-        {
-          auto &segments = tree.segments();
-          auto &segment = segments[i];
-          if (children[i] == 0) // a leaf
-          {
-            // extend the branch
-            Eigen::Vector3d dir = segment.tip - segments[segment.parent_id].tip;
-            double tip_length = dir.norm();
-            dir /= tip_length;
-            
-            // now add a subtree here
-            double initial_branch_length = tip_length;
-            // b. new branch length
-            double new_branch_length = initial_branch_length + prune_length + length_growth;
-
-            Eigen::Vector3d random_dir(ray::randUniformDouble()-0.5, ray::randUniformDouble()-0.5, ray::randUniformDouble()-0.5);
-            Eigen::Vector3d side_dir = dir.cross(random_dir).normalized();
-
-            addSubTree(segments, i, dir, side_dir, new_branch_length,
-              k1, k2, angle1, branch_angle, prune_length);          
-          }
-        }
-
-
-        // 2. get length to end for each sub-branch, and add to a list in order to sort
-        if (shed_option.isSet())
-        {
-          struct ListNode
-          {
-            int segment_id;
-            double distance_to_end;
-          };
-          std::vector<ListNode> nodes;
-          for (size_t i = 0; i<num_segs; i++)
-          {
-            auto &segment = tree.segments()[i];
-            int parent = segment.parent_id;
-            if (parent == -1 || children[parent] > 1) // condition for a sub-branch root
-            {
-              ListNode node;
-              node.segment_id = (int)i;
-              node.distance_to_end = segment.attributes[length_id];
-              nodes.push_back(node);
-            }
-          }
-          std::sort(nodes.begin(), nodes.end(), [](const ListNode &n1, const ListNode &n2) -> bool { return n1.distance_to_end > n2.distance_to_end; });
-          // 3. calculate how much pruning should be done based on dimension
-
-          // 4. go through nodes from longest to shortest, pruning out segments that don't fit the required power law 
-
-        }
-        for (auto &segment : tree.segments())
-        {
-          segment.radius = segment.radius + radius_growth;
+          children[par]++;
         }
       }
+      auto &root = tree.segments()[0];
+      double dimension = tree.treeAttributes()[dimension_id];
+      double branch_angle = root.attributes[angle_id] * ray::kPi/180.0;
+      double radius = root.radius;
+      double length = root.attributes[length_id];
+      double dominance = root.attributes[dominance_id];
+
+      const double radius_growth = length_growth * radius / length;
+
+      // convet dimension to the average downscale at each branch point
+      const double k = std::pow(2.0, -1.0/dimension);
+      // use dominance to work out the large and small downscale...
+      // d1 * d2 = k*k
+      // dominance = -1.0 + 2.0 * sqr(max_rad) / (sqr(max_rad) + sqr(second_max_rad));
+      double area_ratio = (dominance + 1.0) / 2.0; // big child area per parent area
+      double d1 = std::sqrt(area_ratio); // big child radius for unit parent radius
+      double d2 = std::sqrt(1.0 - area_ratio); // small child radius for unit parent radius
+      // if c^2 d1 d2 = k^2
+      double d_scale = k / std::sqrt(d1 * d2);
+      std::cout << "d1: " << d1 << ", d2: " << d2 << ", d_scale: " << d_scale << std::endl;
+      double k1 = d1 * d_scale;
+      double k2 = d2 * d_scale;
+
+      // what are branch angles 1 and 2? We know the dominance and overall branch angle...
+      // angle1 + angle2 = branch_angle
+      // tan(angle1)/rad2^2 = tan(angle2)/rad1^2
+      // tan(angle1)/tan(angle2) = (rad2 / rad1)^2
+      double angle1 = branch_angle/2.0;
+      for (int i = 0; i<20; i++) // no closed form expression, so iterate
+      {
+        angle1 = std::atan( std::tan(branch_angle - angle1) * ray::sqr(k2/k1));
+      }  
+      std::cout << "dominant angle: " << angle1 << ", total angle: " << branch_angle << std::endl;
+
+      // 1. add subtrees at each leaf point....
+      size_t num_segs = tree.segments().size();
+      for (size_t i = 0; i<num_segs; i++)
+      {
+        auto &segments = tree.segments();
+        auto &segment = segments[i];
+        if (children[i] == 0) // a leaf
+        {
+          // extend the branch
+          Eigen::Vector3d dir = segment.tip - segments[segment.parent_id].tip;
+          double tip_length = dir.norm();
+          dir /= tip_length;
+          
+          // now add a subtree here
+          double initial_branch_length = tip_length;
+          // b. new branch length
+          double new_branch_length = initial_branch_length + prune_length + length_growth;
+
+          Eigen::Vector3d random_dir(ray::randUniformDouble()-0.5, ray::randUniformDouble()-0.5, ray::randUniformDouble()-0.5);
+          Eigen::Vector3d side_dir = dir.cross(random_dir).normalized();
+
+          addSubTree(segments, i, dir, side_dir, new_branch_length,
+            k1, k2, angle1, branch_angle, prune_length);          
+        }
+      }
+
+
+      // 2. get length to end for each sub-branch, and add to a list in order to sort
+      if (shed_option.isSet())
+      {
+        struct ListNode
+        {
+          int segment_id;
+          double distance_to_end;
+        };
+        std::vector<ListNode> nodes;
+        for (size_t i = 0; i<num_segs; i++)
+        {
+          auto &segment = tree.segments()[i];
+          int parent = segment.parent_id;
+          if (parent == -1 || children[parent] > 1) // condition for a sub-branch root
+          {
+            ListNode node;
+            node.segment_id = (int)i;
+            node.distance_to_end = segment.attributes[length_id];
+            nodes.push_back(node);
+          }
+        }
+        std::sort(nodes.begin(), nodes.end(), [](const ListNode &n1, const ListNode &n2) -> bool { return n1.distance_to_end > n2.distance_to_end; });
+        // 3. calculate how much pruning should be done based on dimension
+
+        // 4. go through nodes from longest to shortest, pruning out segments that don't fit the required power law 
+      }
+      for (auto &segment : tree.segments())
+      {
+        segment.radius = segment.radius + radius_growth;
+      }
+      // Lastly, we need to iterate through the segments and return them to roughly the original cylinder width to length ratio. Otherwise we'll get lots of short fat cylinders
     }
     grown_forest = forest;
   }
