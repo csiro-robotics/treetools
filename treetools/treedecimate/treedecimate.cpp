c380a9a99d20e3b16d8cd45941b12ac0a155c548
Thomas Lowe <thomas.lowe@csiro.au>
2023-05-15 13:10:38 +1000

    Added a ratio-based tree decimation

--- treetools/treedecimate/treedecimate.cpp
+++ treetools/treedecimate/treedecimate.cpp
@@ -19,6 +19,7 @@ void usage(int exit_code = 1)
   std::cout << "Decimate the segments in the tree file, maintaining the topology and branch location geometry" << std::endl;
   std::cout << "usage:" << std::endl;
   std::cout << "treedecimate forest.txt 2 segments - reduce to only every 2 segments, so roughly half the complexity" << std::endl;
+  std::cout << "treedecimate forest.txt ratio 3    - reduce to the specified maximum length-per-width" << std::endl;
   // clang-format on
   exit(exit_code);
 }
@@ -28,10 +29,12 @@ void usage(int exit_code = 1)
 int main(int argc, char *argv[])
 {
   ray::FileArgument forest_file;
-  ray::TextArgument segments_text("segments");
+  ray::TextArgument segments_text("segments"), ratio_text("ratio");
   ray::IntArgument decimation;
-  const bool parsed = ray::parseCommandLine(argc, argv, { &forest_file, &decimation, &segments_text });
-  if (!parsed)
+  ray::DoubleArgument ratio(0.0, 1000.0);
+  const bool decimate_segments = ray::parseCommandLine(argc, argv, { &forest_file, &decimation, &segments_text });
+  const bool decimate_ratio    = ray::parseCommandLine(argc, argv, { &forest_file, &ratio_text, &ratio });
+  if (!decimate_segments && !decimate_ratio)
   {
     usage();
   }
@@ -48,7 +51,6 @@ int main(int argc, char *argv[])
   }
   ray::ForestStructure new_forest = forest;
 
-  // now, for each segment, we find the neighbours in range
   for (size_t t = 0; t < forest.trees.size(); t++)
   {
     auto &tree = forest.trees[t];
@@ -58,28 +60,47 @@ int main(int argc, char *argv[])
     {
       children[tree.segments()[i].parent_id].push_back(static_cast<int>(i));
     }
-    std::vector<int> new_index(tree.segments().size());
-    std::vector<int> counts(tree.segments().size());  // count up from each branch point
-    new_index[0] = 0;
-    counts[0] = 0;
-
     auto &new_tree = new_forest.trees[t];
-    new_tree.segments().clear();
-    new_tree.segments().push_back(tree.segments()[0]);
-    for (size_t i = 1; i < tree.segments().size(); i++)
+
+    if (decimate_segments)
     {
-      counts[i] = counts[tree.segments()[i].parent_id] + 1;
-      if (counts[i] == decimation.value() || children[i].size() > 1 || children[i].size() == 0)
+      new_tree.segments().clear();
+      new_tree.segments().push_back(tree.segments()[0]);
+      std::vector<int> new_index(tree.segments().size());
+      std::vector<int> counts(tree.segments().size());  // count up from each branch point
+      new_index[0] = 0;
+      counts[0] = 0;
+      for (size_t i = 1; i < tree.segments().size(); i++)
       {
-        new_index[i] = static_cast<int>(new_tree.segments().size());
-        new_tree.segments().push_back(tree.segments()[i]);
-        new_tree.segments().back().parent_id = new_index[tree.segments()[i].parent_id];
-        counts[i] = 0;
+        counts[i] = counts[tree.segments()[i].parent_id] + 1;
+        if (counts[i] == decimation.value() || children[i].size() > 1 || children[i].size() == 0)
+        {
+          new_index[i] = static_cast<int>(new_tree.segments().size());
+          new_tree.segments().push_back(tree.segments()[i]);
+          new_tree.segments().back().parent_id = new_index[tree.segments()[i].parent_id];
+          counts[i] = 0;
+        }
+        else
+        {
+          new_index[i] = new_index[tree.segments()[i].parent_id];
+        }
       }
-      else
+    }
+    else // decimate ratio
+    {
+      for (size_t i = 1; i < new_tree.segments().size(); i++)
       {
-        new_index[i] = new_index[tree.segments()[i].parent_id];
+        auto &segment = new_tree.segments()[i];
+        double length = (segment.tip - new_tree.segments()[segment.parent_id].tip).norm();
+        double width = 2.0 * segment.radius;
+        if (children[i].size() == 1 && length < ratio.value() * width) 
+        {
+          // we have to remove this segment...
+          new_tree.segments()[children[i][0]].parent_id = segment.parent_id;
+          segment.parent_id = -1; // mark as unused, for later reindexing
+        }
       }
+      new_tree.reindex();
     }
   }
   new_forest.save(forest_file.nameStub() + "_decimated.txt");
