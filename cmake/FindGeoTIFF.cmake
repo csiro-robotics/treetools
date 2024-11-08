e299021be1586645a8fb2c7e3b426183e77bb00e
Tim Devereux <devereux.tj@gmail.com>
2024-11-08 12:15:00 +1000

    better handling of GeoTIFF and PROJ

--- cmake/FindGeoTIFF.cmake
+++ cmake/FindGeoTIFF.cmake
@@ -1,71 +1,29 @@
-###############################################################################
-#
-# CMake module to search for GeoTIFF library
-#
-# On success, the macro sets the following variables:
-# GEOTIFF_FOUND       = if the library found
-# GEOTIFF_LIBRARIES   = full path to the library
-# GEOTIFF_INCLUDE_DIR = where to find the library headers 
-# also defined, but not for general use are
-# GEOTIFF_LIBRARY, where to find the PROJ.4 library.
-#
-# Copyright (c) 2009 Mateusz Loskot <mateusz@loskot.net>
-#
-# Module source: http://github.com/mloskot/workshop/tree/master/cmake/
-#
-# Redistribution and use is allowed according to the terms of the BSD license.
-# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
-#
-###############################################################################
+# FindGeoTIFF.cmake
 
-SET(GEOTIFF_NAMES geotiff)
+# Try to find the GeoTIFF library and headers
+find_path(GeoTIFF_INCLUDE_DIR NAMES geotiff.h
+    HINTS
+    ${CMAKE_INSTALL_PREFIX}/include
+    /usr/local/include/geotiff
+    /usr/include
+)
 
-IF(WIN32)
+find_library(GeoTIFF_LIBRARY NAMES geotiff
+    HINTS
+    ${CMAKE_INSTALL_PREFIX}/lib
+    /usr/local/lib
+    /usr/lib
+    /usr/lib/x86_64-linux-gnu
+)
 
-    IF(MINGW)
-        FIND_PATH(GEOTIFF_INCLUDE_DIR
-            geotiff.h
-            PATH_PREFIXES geotiff
-            PATHS
-            /usr/local/include
-            /usr/include
-            c:/msys/local/include)
+# Handle the result
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(GeoTIFF REQUIRED_VARS GeoTIFF_INCLUDE_DIR GeoTIFF_LIBRARY)
 
-        FIND_LIBRARY(GEOTIFF_LIBRARY
-            NAMES ${GEOTIFF_NAMES}
-            PATHS
-            /usr/local/lib
-            /usr/lib
-            c:/msys/local/lib)
-    ENDIF(MINGW)
+if(GeoTIFF_FOUND)
+    set(GeoTIFF_LIBRARIES ${GeoTIFF_LIBRARY})
+else()
+    set(GeoTIFF_LIBRARIES "")
+endif()
 
-    IF(MSVC)
-        SET(GEOTIFF_INCLUDE_DIR "$ENV{LIB_DIR}/include" CACHE STRING INTERNAL)
-
-        SET(GEOTIFF_NAMES ${GEOTIFF_NAMES} geotiff_i)
-        FIND_LIBRARY(GEOTIFF_LIBRARY NAMES 
-            NAMES ${GEOTIFF_NAMES}
-            PATHS
-            "$ENV{LIB_DIR}/lib"
-            /usr/lib
-            c:/msys/local/lib)
-    ENDIF(MSVC)
-  
-ELSEIF(UNIX)
-
-    FIND_PATH(GEOTIFF_INCLUDE_DIR geotiff.h PATH_PREFIXES geotiff)
-
-    FIND_LIBRARY(GEOTIFF_LIBRARY NAMES ${GEOTIFF_NAMES})
-
-ELSE()
-    MESSAGE("FindGeoTIFF.cmake: unrecognized or unsupported operating system")
-ENDIF()
-
-IF(GEOTIFF_FOUND)
-  SET(GEOTIFF_LIBRARIES ${GEOTIFF_LIBRARY})
-ENDIF()
-
-# Handle the QUIETLY and REQUIRED arguments and set SPATIALINDEX_FOUND to TRUE
-# if all listed variables are TRUE
-INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(GEOTIFF DEFAULT_MSG GEOTIFF_LIBRARY GEOTIFF_INCLUDE_DIR)
+mark_as_advanced(GeoTIFF_INCLUDE_DIR GeoTIFF_LIBRARY)
